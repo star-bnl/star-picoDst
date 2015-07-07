29a08aa471d8d8bd7a2f2975fe1c09967d90a3f9
Mustafa Mustafa <mstftsm@gmail.com>
2015-07-07 13:08:59 -0700

    We don't create TFile on the heap

--- StPicoDstMaker/StPicoDstMaker.cxx
+++ StPicoDstMaker/StPicoDstMaker.cxx
@@ -331,15 +331,13 @@ Int_t StPicoDstMaker::openRead() {
       inputStream.getline(line,512);
       string aFile = line;      
       if (inputStream.good() && aFile.find(".picoDst.root")!=string::npos) {
-//        TFile *ftmp = new TFile(line);
-        TFile *ftmp = TFile::Open(line);
-        if(ftmp && ftmp->IsOpen() && ftmp->GetNkeys()) {
+
+        TFile ftmp(line);
+        if(!ftmp.IsZombie() && ftmp.GetNkeys()) {
           LOG_INFO << " Read in picoDst file " << line << endm;
           mChain->Add(line);
           nFile++;
         }
-
-        delete ftmp;
       }
     }
     LOG_INFO << " Total " << nFile << " files have been read in. " << endm;
