469eb553ee44799f72d6198dc12680cc0c02993a
Mustafa Mustafa <mstftsm@gmail.com>
2016-03-14 09:10:40 -0700

    as per the guidelines, methods should be defined outside the class header.

--- StPicoDstMaker/StPicoBTofHit.h
+++ StPicoDstMaker/StPicoBTofHit.h
@@ -14,11 +14,10 @@ class StPicoBTofHit : public TObject {
   void    Clear(const Option_t *opt="");
   virtual void Print(const Char_t *option = "") const;  ///< Print trigger info
  
-  Int_t   id() const             { return (Int_t)mId; }
-  Int_t   tray() const           { return (Int_t)mId/192 + 1; }
-  Int_t   module() const         { return ((Int_t)mId%192)/6 + 1; }
-  Int_t   cell() const           { return (Int_t)mId/6 + 1; }
-    
+  Int_t   id() const;
+  Int_t   tray() const;
+  Int_t   module() const;
+  Int_t   cell() const;
 
  protected:
   Short_t mId;     // (tray-1)*192+(module-1)*6+(cell-1):
@@ -28,4 +27,8 @@ class StPicoBTofHit : public TObject {
   ClassDef(StPicoBTofHit, 1)
 };
 
+inline Int_t StPicoBTofHit::id() const     { return (Int_t)mId; }
+inline Int_t StPicoBTofHit::tray() const   { return (Int_t)mId/192 + 1; }
+inline Int_t StPicoBTofHit::module() const { return ((Int_t)mId%192)/6 + 1; }
+inline Int_t StPicoBTofHit::cell() const   { return (Int_t)mId/6 + 1; }
 #endif
