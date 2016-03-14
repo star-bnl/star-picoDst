30850da807fa5a8759249c2c6e2765223be4b300
Mustafa Mustafa <mstftsm@gmail.com>
2016-03-14 09:19:56 -0700

    as per the guidelines, methods should be define outside the header

--- StPicoDstMaker/StPicoBTOWHit.h
+++ StPicoDstMaker/StPicoBTOWHit.h
@@ -14,9 +14,9 @@ class StPicoBTOWHit : public TObject {
   void    Clear(const Option_t *opt="");
   virtual void Print(const Char_t *option = "") const;  ///< Print trigger info
  
-  Int_t   id() const             { return (Int_t)mId; }
-  Int_t   adc() const            { return (Int_t)mAdc; }
-  Float_t energy() const         { return (Float_t)mE/1000.; }
+  Int_t   id() const;
+  Int_t   adc() const;
+  Float_t energy() const;
 
  protected:
   UShort_t mId;    // towerId 1-4800
@@ -29,4 +29,7 @@ class StPicoBTOWHit : public TObject {
   ClassDef(StPicoBTOWHit, 1)
 };
 
+inline Int_t   StPicoBTOWHit::id() const     { return (Int_t)mId; }
+inline Int_t   StPicoBTOWHit::adc() const    { return (Int_t)mAdc; }
+inline Float_t StPicoBTOWHit::energy() const { return (Float_t)mE/1000.; }
 #endif
