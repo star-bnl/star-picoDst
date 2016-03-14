792116c8a9721e8fd2bdd891f5e3c6e05350a2d1
Mustafa Mustafa <mstftsm@gmail.com>
2016-03-14 09:30:13 -0700

    as per guidelines, methods should be defined out side of the header

--- StPicoDstMaker/StPicoMtdHit.h
+++ StPicoDstMaker/StPicoMtdHit.h
@@ -14,18 +14,17 @@ class StPicoMtdHit : public TObject {
   ~StPicoMtdHit();
 
   virtual void Print(const Char_t *option = "") const; 
-  void   setTriggerFlag(const Int_t flag)  { mTriggerFlag = (UChar_t)flag; }
 
-  Int_t  gChannel()    const { return (Int_t)mgChannel;             }
-  Int_t  backleg()     const { return (Int_t)mgChannel/60 + 1;      }
-  Int_t  module()      const { return ((Int_t)mgChannel%60)/12 + 1; }
-  Int_t  cell()        const { return (Int_t)mgChannel%12;          }
-  Int_t  triggerFlag() const { return (Int_t) mTriggerFlag;         }   
+  void  setTriggerFlag(Int_t const flag);
+  Int_t  gChannel()    const;
+  Int_t  backleg()     const;
+  Int_t  module()      const;
+  Int_t  cell()        const;
+  Int_t  triggerFlag() const;
   
-  pair<Float_t,Float_t> leadingEdgeTime()  const { return mLeadingEdgeTime;  }
-  pair<Float_t,Float_t> trailingEdgeTime() const { return mTrailingEdgeTime; }
-  pair<Float_t,Float_t> tot() const 
-  { return pair<Float_t,Float_t>(mTrailingEdgeTime.first - mLeadingEdgeTime.first, mTrailingEdgeTime.second - mLeadingEdgeTime.second); }
+  pair<Float_t,Float_t> leadingEdgeTime()  const;
+  pair<Float_t,Float_t> trailingEdgeTime() const;
+  pair<Float_t,Float_t> tot() const;
   
  protected:
   Short_t mgChannel; // mgChannel = (backleg-1) * 60 + (module-1) * 12 + cell
@@ -34,7 +33,15 @@ class StPicoMtdHit : public TObject {
   pair<Float_t,Float_t>  mTrailingEdgeTime;
 	
   ClassDef(StPicoMtdHit,1)
-
 };
-
+inline void StPicoMtdHit::setTriggerFlag(Int_t const flag) { mTriggerFlag = (UChar_t)flag;}
+inline Int_t StPicoMtdHit::gChannel()    const { return (Int_t)mgChannel;             }
+inline Int_t StPicoMtdHit::backleg()     const { return (Int_t)mgChannel/60 + 1;      }
+inline Int_t StPicoMtdHit::module()      const { return ((Int_t)mgChannel%60)/12 + 1; }
+inline Int_t StPicoMtdHit::cell()        const { return (Int_t)mgChannel%12;          }
+inline Int_t StPicoMtdHit::triggerFlag() const { return (Int_t) mTriggerFlag;         }
+inline pair<Float_t,Float_t> StPicoMtdHit::leadingEdgeTime()  const { return mLeadingEdgeTime;  }
+inline pair<Float_t,Float_t> StPicoMtdHit::trailingEdgeTime() const { return mTrailingEdgeTime; }
+inline pair<Float_t,Float_t> StPicoMtdHit::tot() const
+{ return pair<Float_t,Float_t>(mTrailingEdgeTime.first - mLeadingEdgeTime.first, mTrailingEdgeTime.second - mLeadingEdgeTime.second); }
 #endif
