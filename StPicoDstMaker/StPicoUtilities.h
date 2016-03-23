81b21e1b23834935aca7c0c028b75595e061301e
Ji Xu <ccnuxuji@gmail.com>
2016-03-22 21:46:59 -0400

    Added basic version for refmult3 calculation

--- StPicoDstMaker/StPicoUtilities.h
+++ StPicoDstMaker/StPicoUtilities.h
@@ -86,7 +86,40 @@ namespace StPicoUtilities {
     return countedTracks;
   }
 
+  //______________________________________________________________________________
+  // Reference multiplicity3 |eta|<1 (Pions and Kaons)
+  inline UInt_t refMult3(const UInt_t charge, const UInt_t etaId, const StMuDst& muDst)
+  {
+    // charge   0:negative, 1:positive
+    // etaId    0:East(-1<eta<0), 1:West(1>eta>0)
+
+    size_t countedTracks = 0;
+    for (Int_t itrk=0; itrk<muDst.primaryTracks()->GetEntries(); itrk++){
+      StMuTrack* track = muDst.primaryTracks(itrk) ;
+      if(!track) continue;
+
+      // these first 3 checks are easy, save time
+     // const Bool_t isChargeOk = (charge==0&&track->charge()<0)||(charge==1&&track->charge()>0);
+     // if (track->flag()<0 || !isChargeOk || track->nHitsFit(kTpcId)<10 ) continue; 
+
+      // check eta, a bit more elaborate
+     // if (fabs(track->momentum().mag())<1.e-10) continue;
+     // const Double_t eta = track->momentum().pseudoRapidity() ;
+     // const Bool_t isEtaOk = (etaId==0&&(eta>-1.0&&eta<-0.5)) || (etaId==1&&(eta>0.5&&eta<1.0));
+     // if (!isEtaOk) continue;
+      // finally, check dca, if a track satisfies gets inside the if, count it.
+      //if (track->dca().mag()<3) ++countedTracks;
+    }
+    return countedTracks;
+  }
+
+
+
 }
 
 #endif
+
+
+
+
 
