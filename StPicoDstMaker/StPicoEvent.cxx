3f2a11541d22bd3b2c18f774cc19ed9879d7019b
Mustafa Mustafa <mstftsm@gmail.com>
2016-03-24 09:46:47 -0700

    streamline the data members. Initialize variables in initializer list. Delegate construction.

--- StPicoDstMaker/StPicoEvent.cxx
+++ StPicoDstMaker/StPicoEvent.cxx
@@ -1,12 +1,13 @@
 #include <algorithm>
+#include <limits>
 #include "StPicoEvent.h"
 #include "StPicoConstants.h"
 #include "StEvent/StEventTypes.h"
 #include "St_base/StTree.h"
 #include "StEventUtilities/StuRefMult.hh"
 #include "TVector2.h"
-#include "StMuDSTMaker/COMMON/StMuDst.h" 
-#include "StMuDSTMaker/COMMON/StMuTrack.h" 
+#include "StMuDSTMaker/COMMON/StMuDst.h"
+#include "StMuDSTMaker/COMMON/StMuTrack.h"
 #include "StMuDSTMaker/COMMON/StMuEvent.h"
 #include "StMuDSTMaker/COMMON/StMuPrimaryVertex.h"
 #include "StMuDSTMaker/COMMON/StMuMtdHeader.h"
@@ -16,13 +17,29 @@
 
 ClassImp(StPicoEvent)
 
-StPicoEvent::StPicoEvent()
+StPicoEvent::StPicoEvent():
+ mRunId(0), mEventId(0), mFillId(0), mBField(0),
+ mPrimaryVertex{-999.,-999.,-999.}, mPrimaryVertexError{-999.,-999.,-999},
+ mRanking(-999), mNBEMCMatch(0), mNBTOFMatch(0),
+ mTriggerIds{},
+ mRefMultFtpcEast(0), mRefMultFtpcWest(0),
+ mRefMultNeg(0), mRefMultPos(0),
+ mRefMult2NegEast(0), mRefMult2PosEast(0), mRefMult2NegWest(0), mRefMult2PosWest(0),
+ mRefMultHalfNegEast(0), mRefMultHalfPosEast(0), mRefMultHalfNegWest(0), mRefMultHalfPosWest(0),
+ mGRefMult(0), mNumberOfGlobalTracks(0), mbTofTrayMultiplicity(0), mNHitsHFT{},
+ mNVpdHitsEast(0), mNVpdHitsWest(0), mNT0(0), mVzVpd(std::numeric_limits<short>::max()),
+ mZDCx(0), mBBCx(0), mBackgroundRate(0), mBbcBlueBackgroundRate(0), mBbcYellowBackgroundRate(0),
+ mBbcEastRate(0), mBbcWestRate(0), mZdcEastRate(0), mZdcWestRate(0),
+ mVpd{}, mZdcSumAdcEast(0), mZdcSumAdcWest(0),
+ mZdcSmdEastHorizontal{}, mZdcSmdEastVertical{}, mZdcSmdWestHorizontal{}, mZdcSmdWestVertical{},
+ mBbcAdcEast{}, mBbcAdcWest{},
+ mSpaceCharge(0),
+ mHT_Th{}, mJP_Th{}
 {}
 
-StPicoEvent::StPicoEvent(const StMuDst& muDst)
+StPicoEvent::StPicoEvent(const StMuDst& muDst) : StPicoEvent()
 {
   StMuEvent* ev = muDst.event() ;
-  StBTofHeader* header = muDst.btofHeader() ;
 
   mRunId = ev->runNumber();
   mEventId = ev->eventNumber();
@@ -31,9 +48,17 @@ StPicoEvent::StPicoEvent(const StMuDst& muDst)
 
   mPrimaryVertex = ev->primaryVertexPosition();
   mPrimaryVertexError = ev->primaryVertexErrors();
-  if( mPrimaryVertex.x()==mPrimaryVertex.y()&&mPrimaryVertex.y()==mPrimaryVertex.z() ){
+  if( mPrimaryVertex.x()==mPrimaryVertex.y() && mPrimaryVertex.y()==mPrimaryVertex.z() )
+  {
     mPrimaryVertex.set(-999.,-999.,-999.);
     mPrimaryVertexError.set(0.,0.,0);
+  }
+
+  if(StMuPrimaryVertex* pv = muDst.primaryVertex())
+  {
+    mRanking = pv->ranking();
+    mNBEMCMatch = pv->nBEMCMatch();
+    mNBTOFMatch = pv->nBTOFMatch();
   }
 
   mTriggerIds = ev->triggerIdCollection().nominal().triggerIds();
@@ -50,110 +75,82 @@ StPicoEvent::StPicoEvent(const StMuDst& muDst)
   mRefMultHalfPosEast = (UShort_t)StPicoUtilities::refMultHalf(1, 0, muDst);
   mRefMultHalfNegWest = (UShort_t)StPicoUtilities::refMultHalf(0, 1, muDst);
   mRefMultHalfPosWest = (UShort_t)StPicoUtilities::refMultHalf(1, 1, muDst);
+
   mGRefMult = (UShort_t)ev->grefmult();
+  mNumberOfGlobalTracks = muDst.numberOfGlobalTracks();
+  mbTofTrayMultiplicity = ev->btofTrayMultiplicity() ;
   mNHitsHFT[0] = (UShort_t)ev->numberOfPxlInnerHits();
   mNHitsHFT[1] = (UShort_t)ev->numberOfPxlOuterHits();
   mNHitsHFT[2] = (UShort_t)ev->numberOfIstHits();
   mNHitsHFT[3] = (UShort_t)ev->numberOfSsdHits();
 
-
-  LOG_DEBUG << "Tmp: Neg: RefMult(org) = " << mRefMultNeg << "  (new) = " << StPicoUtilities::refMult(0, muDst)
-    << " refmult2 = " << mRefMult2NegEast+ mRefMult2PosEast+ mRefMult2NegWest+ mRefMult2PosWest
-    << " refmulthalf(<0) = " << mRefMultHalfNegEast+ mRefMultHalfPosEast
-    << " refmulthalf(>0) = " << mRefMultHalfNegWest+ mRefMultHalfPosWest
-    << endm;
-
-  mNVpdHitsEast = 0;
-  mNVpdHitsWest = 0;
-  mNT0 = 0;
-  mVzVpd = Pico::SHORTMAX;
-  if(header) {
+  if(StBTofHeader* header = muDst.btofHeader())
+  {
     mNVpdHitsEast = (UChar_t)(header->numberOfVpdHits(east));
     mNVpdHitsWest = (UChar_t)(header->numberOfVpdHits(west));
     mNT0 = (UShort_t)(header->nTzero());
     mVzVpd = (fabs(header->vpdVz()*100.)>Pico::SHORTMAX) ? Pico::SHORTMAX : (UShort_t)(TMath::Nint(header->vpdVz()*100.));
   }
 
-  mbTofTrayMultiplicity = ev->btofTrayMultiplicity() ;
-  mNumberOfGlobalTracks = muDst.numberOfGlobalTracks() ;
-
-  StMuPrimaryVertex* pv = muDst.primaryVertex() ;
-  if(pv){
-    mRanking = pv->ranking() ;
-    mNBEMCMatch = pv->nBEMCMatch() ;
-    mNBTOFMatch = pv->nBTOFMatch() ;
+  //Nov.10, 2008, Na
+  StZdcTriggerDetector& ZDC = ev->zdcTriggerDetector();
+  mZdcSumAdcEast = (UShort_t)ZDC.adcSum(east);
+  mZdcSumAdcWest = (UShort_t)ZDC.adcSum(west);
+  for (int strip=1;strip<9;++strip)
+  {
+    if (ZDC.zdcSmd(east,1,strip))
+      mZdcSmdEastHorizontal[strip-1] = (UShort_t)ZDC.zdcSmd(east,1,strip);
+    if (ZDC.zdcSmd(east,0,strip))
+      mZdcSmdEastVertical[strip-1] = (UShort_t)ZDC.zdcSmd(east,0,strip);
+    if (ZDC.zdcSmd(west,1,strip))
+      mZdcSmdWestHorizontal[strip-1] = (UShort_t)ZDC.zdcSmd(west,1,strip);
+    if (ZDC.zdcSmd(west,0,strip))
+      mZdcSmdWestVertical[strip-1] = (UShort_t)ZDC.zdcSmd(west,0,strip);
   }
-  else{
-    mRanking = -999.;
-    mNBEMCMatch = 0;
-    mNBTOFMatch = 0;
+
+  StVpdTriggerDetector& VPD = ev->vpdTriggerDetector();
+
+  for(int i=0; i<16; ++i)
+  {
+    //event.VPD[i]= 1.0*theVPD.adc(i);
+    if(i>=0 && i<8) {
+      mVpd[i]=(UShort_t)VPD.ADC(east,8-i);
+      mVpd[i+8]=(UShort_t)VPD.TDC(east,8-i);
+      mVpd[i+32]=(UShort_t)VPD.ADC(west,8-i);
+      mVpd[i+40]=(UShort_t)VPD.TDC(west,8-i);
+    }
+    if(i>=8 && i<16) {
+      mVpd[i+8]=(UShort_t)VPD.ADC(east,32-(i+8));
+      mVpd[i+16]=(UShort_t)VPD.TDC(east,32-(i+8));
+      mVpd[i+40]=(UShort_t)VPD.ADC(west,32-(i+8));
+      mVpd[i+48]=(UShort_t)VPD.TDC(west,32-(i+8));
+      //cout<<"VPD-------  "<<VPD.ADC(east,32-(i+8))<<endl;
+    }
   }
 
+  mZDCx = (UInt_t)ev->runInfo().zdcCoincidenceRate();
+  mBBCx = (UInt_t)ev->runInfo().bbcCoincidenceRate();
+  mBackgroundRate = ev->runInfo().backgroundRate();
+  mBbcBlueBackgroundRate = ev->runInfo().bbcBlueBackgroundRate();
+  mBbcYellowBackgroundRate = ev->runInfo().bbcYellowBackgroundRate();
+  mBbcEastRate = ev->runInfo().bbcEastRate();
+  mBbcWestRate = ev->runInfo().bbcWestRate();
+  mZdcEastRate = ev->runInfo().zdcEastRate();
+  mZdcWestRate = ev->runInfo().zdcWestRate();
 
-    //Nov.10, 2008, Na
-    StZdcTriggerDetector &ZDC = ev->zdcTriggerDetector();
-    mZdcSumAdcEast = (UShort_t)ZDC.adcSum(east);
-    mZdcSumAdcWest = (UShort_t)ZDC.adcSum(west);   
-    for (int strip=1;strip<9;strip++) {
-        if (ZDC.zdcSmd(east,1,strip)) 
-          mZdcSmdEastHorizontal[strip-1] = (UShort_t)ZDC.zdcSmd(east,1,strip);
-        if (ZDC.zdcSmd(east,0,strip))  
-          mZdcSmdEastVertical[strip-1] = (UShort_t)ZDC.zdcSmd(east,0,strip);
-        if (ZDC.zdcSmd(west,1,strip)) 
-          mZdcSmdWestHorizontal[strip-1] = (UShort_t)ZDC.zdcSmd(west,1,strip);
-        if (ZDC.zdcSmd(west,0,strip))  
-          mZdcSmdWestVertical[strip-1] = (UShort_t)ZDC.zdcSmd(west,0,strip);
-    }
 
-    StVpdTriggerDetector &VPD = ev->vpdTriggerDetector();
+  // BBC ADC (Hiroshi)
+  StBbcTriggerDetector bbc = ev->bbcTriggerDetector() ;
+  for(UInt_t i=0;i<bbc.numberOfPMTs();++i)
+  {
+    const UInt_t eastWest = (i<24) ? 0 : 1 ; // East:0-23, West:24-47
+    const UInt_t pmtId    = i%24 ;           // pmtId:0-23
 
-    for(int i=0; i<16; i++){
-      //event.VPD[i]= 1.0*theVPD.adc(i);
-      if(i>=0 && i<8) {
-        mVpd[i]=(UShort_t)VPD.ADC(east,8-i);
-        mVpd[i+8]=(UShort_t)VPD.TDC(east,8-i);
-        mVpd[i+32]=(UShort_t)VPD.ADC(west,8-i);
-        mVpd[i+40]=(UShort_t)VPD.TDC(west,8-i);
-      }
-      if(i>=8 && i<16) {
-        mVpd[i+8]=(UShort_t)VPD.ADC(east,32-(i+8));
-        mVpd[i+16]=(UShort_t)VPD.TDC(east,32-(i+8));
-        mVpd[i+40]=(UShort_t)VPD.ADC(west,32-(i+8));
-        mVpd[i+48]=(UShort_t)VPD.TDC(west,32-(i+8));
-        //cout<<"VPD-------  "<<VPD.ADC(east,32-(i+8))<<endl;
-      }
-    }
+    if( eastWest == 0 ) mBbcAdcEast[pmtId] = bbc.adc(i) ;
+    else                mBbcAdcWest[pmtId] = bbc.adc(i) ;
+  }
 
-    mZDCx = (UInt_t)ev->runInfo().zdcCoincidenceRate();
-    mBBCx = (UInt_t)ev->runInfo().bbcCoincidenceRate();
-
-    mBackgroundRate = ev->runInfo().backgroundRate();
-
-    mBbcBlueBackgroundRate = ev->runInfo().bbcBlueBackgroundRate();
-    mBbcYellowBackgroundRate = ev->runInfo().bbcYellowBackgroundRate();
-
-    mBbcEastRate = ev->runInfo().bbcEastRate();
-    mBbcWestRate = ev->runInfo().bbcWestRate();
-
-    mZdcEastRate = ev->runInfo().zdcEastRate();
-    mZdcWestRate = ev->runInfo().zdcWestRate();
-
-    mSpaceCharge = ev->runInfo().spaceCharge();
-
-    // BBC ADC (Hiroshi)
-    StBbcTriggerDetector bbc = ev->bbcTriggerDetector() ;
-    for(UInt_t i=0;i<bbc.numberOfPMTs();i++){
-      const UInt_t eastWest = (i<24) ? 0 : 1 ; // East:0-23, West:24-47
-      const UInt_t pmtId    = i%24 ;           // pmtId:0-23
-
-      if( eastWest == 0 ) mBbcAdcEast[pmtId] = bbc.adc(i) ;
-      else                mBbcAdcWest[pmtId] = bbc.adc(i) ;
-    }
-
-    for(int i=0;i<4;i++) {
-      setHT_Th(i,0);
-      setJP_Th(i,0);
-    }
+  mSpaceCharge = ev->runInfo().spaceCharge();
 }
 
 StPicoEvent::~StPicoEvent()
--- StPicoDstMaker/StPicoEvent.h
+++ StPicoDstMaker/StPicoEvent.h
@@ -17,15 +17,21 @@ public:
   StPicoEvent();
   virtual ~StPicoEvent();
   StPicoEvent(const StMuDst& muDst);
-  
+
   Int_t    runId() const;
   Int_t    eventId() const;
   Int_t    fillId() const;
   Float_t  bField() const;
-  StThreeVectorF const & primaryVertex() const;
-  StThreeVectorF const & primaryVertexError() const;
+
+  StThreeVectorF const& primaryVertex() const;
+  StThreeVectorF const& primaryVertexError() const;
+  Float_t               ranking() const;
+  UShort_t              nBEMCMatch() const;
+  UShort_t              nBTOFMatch() const;
+
   std::vector<unsigned int> triggerIds() const;
-  bool     isTrigger(unsigned int) const;
+  bool                      isTrigger(unsigned int) const;
+
   Int_t    refMultPos() const;
   Int_t    refMultNeg() const;
   Int_t    refMultFtpcEast() const;
@@ -45,17 +51,20 @@ public:
   Int_t    refMult2() const;
   Int_t    refMultHalfEast() const;
   Int_t    refMultHalfWest() const;
+
   Int_t    grefMult() const;
+  UShort_t numberOfGlobalTracks() const;
+  UShort_t btofTrayMultiplicity() const;
   Int_t    numberOfPxlInnerHits() const;
   Int_t    numberOfPxlOuterHits() const;
   Int_t    numberOfIstHits() const;
   Int_t    numberOfSsdHits() const;
-        
+
   Int_t    nVpdHitsEast() const;
   Int_t    nVpdHitsWest() const;
   Int_t    nT0() const;
   Float_t  vzVpd() const;
-  
+
   Float_t  ZDCx() const;
   Float_t  BBCx() const;
   Float_t  Vpd(int i) const;
@@ -73,61 +82,61 @@ public:
   Float_t bbcWestRate() const;
   Float_t zdcEastRate() const;
   Float_t zdcWestRate() const;
-  
-  Float_t spaceCharge() const;
 
-  UShort_t btofTrayMultiplicity() const;
-  UShort_t numberOfGlobalTracks() const;
-
-  Float_t ranking() const;
-  UShort_t nBEMCMatch() const;
-  UShort_t nBTOFMatch() const;
-  
   UShort_t bbcAdcEast(const Int_t i) const;
   UShort_t bbcAdcWest(const Int_t i) const;
 
+  Float_t spaceCharge() const;
+
   Int_t   ht_th(const Int_t i) const;
   Int_t   jp_th(const Int_t i) const;
-  
-// other user's functions
+
   int      year() const;
   int      day() const;
 
 // set functions for trigger thresholds
   void     setHT_Th(const Int_t i, const Int_t th);
   void     setJP_Th(const Int_t i, const Int_t th);
-      
-protected: //these are written out
-  Int_t          mRunId;           // run number
-  Int_t          mEventId;         // event number
-  UShort_t       mFillId;          // fill number
-  Float_t        mBField;          // B field in kilogauss
-  StThreeVectorF mPrimaryVertex;   // primary Vertex (1st)
-  StThreeVectorF mSecondVertex;    // second Vertex position (for study)
-  StThreeVectorF mPrimaryVertexError; // vertex error
-  std::vector<unsigned int> mTriggerIds;
-  UShort_t       mRefMultFtpcEast; // FTPC refMult east
-  UShort_t       mRefMultFtpcWest; // FTPC refMult west
-  UShort_t       mRefMultNeg;      // TPC refMult neg
-  UShort_t       mRefMultPos;      // TPC refMult pos
-  UShort_t       mRefMult2NegEast; // TPC refMult2 neg (-1<eta<-0.5)
-  UShort_t       mRefMult2PosEast; // TPC refMult2 pos (-1<eta<-0.5)
-  UShort_t       mRefMult2NegWest; // TPC refMult2 neg (0.5<eta<1.0)
-  UShort_t       mRefMult2PosWest; // TPC refMult2 pos (0.5<eta<1.0)
-  UShort_t       mRefMultHalfNegEast ;// TPC refMultHalf neg (eta<0)
-  UShort_t       mRefMultHalfPosEast ;// TPC refMultHalf pos (eta<0)
-  UShort_t       mRefMultHalfNegWest ;// TPC refMultHalf neg (eta>0)
-  UShort_t       mRefMultHalfPosWest ;// TPC refMultHalf pos (eta>0)
-  UShort_t       mGRefMult;
-  UShort_t       mNHitsHFT[4];
-  
-  UChar_t        mNVpdHitsEast;    // Vpd Hits east;
-  UChar_t        mNVpdHitsWest;    // vpd hits west;
-  UShort_t       mNT0;             // number of T0 particles in BTOF self calibration
-  Short_t        mVzVpd;           // VzVpd*100.
 
-  UInt_t        mZDCx;           // zdcX
-  UInt_t        mBBCx;
+protected: //these are written out
+  Int_t    mRunId;           // run number
+  Int_t    mEventId;         // event number
+  UShort_t mFillId;          // fill number
+  Float_t  mBField;          // B field in kilogauss
+
+  StThreeVectorF mPrimaryVertex;   // primary Vertex (1st)
+  StThreeVectorF mPrimaryVertexError; // vertex error
+  Float_t mRanking;
+  UShort_t mNBEMCMatch;
+  UShort_t mNBTOFMatch;
+
+  std::vector<unsigned int> mTriggerIds;
+
+  UShort_t mRefMultFtpcEast; // FTPC refMult east
+  UShort_t mRefMultFtpcWest; // FTPC refMult west
+  UShort_t mRefMultNeg;      // TPC refMult neg
+  UShort_t mRefMultPos;      // TPC refMult pos
+  UShort_t mRefMult2NegEast; // TPC refMult2 neg (-1<eta<-0.5)
+  UShort_t mRefMult2PosEast; // TPC refMult2 pos (-1<eta<-0.5)
+  UShort_t mRefMult2NegWest; // TPC refMult2 neg (0.5<eta<1.0)
+  UShort_t mRefMult2PosWest; // TPC refMult2 pos (0.5<eta<1.0)
+  UShort_t mRefMultHalfNegEast ;// TPC refMultHalf neg (eta<0)
+  UShort_t mRefMultHalfPosEast ;// TPC refMultHalf pos (eta<0)
+  UShort_t mRefMultHalfNegWest ;// TPC refMultHalf neg (eta>0)
+  UShort_t mRefMultHalfPosWest ;// TPC refMultHalf pos (eta>0)
+
+  UShort_t mGRefMult;
+  UShort_t mNumberOfGlobalTracks ; // # of global tracks
+  UShort_t mbTofTrayMultiplicity ; // BTOF tray multiplicity
+  UShort_t mNHitsHFT[4];
+
+  UChar_t  mNVpdHitsEast;    // Vpd Hits east;
+  UChar_t  mNVpdHitsWest;    // vpd hits west;
+  UShort_t mNT0;             // number of T0 particles in BTOF self calibration
+  Short_t  mVzVpd;           // VzVpd*100.
+
+  UInt_t  mZDCx;           // zdcX
+  UInt_t  mBBCx;
   Float_t mBackgroundRate;
   Float_t mBbcBlueBackgroundRate;
   Float_t mBbcYellowBackgroundRate;
@@ -135,6 +144,7 @@ protected: //these are written out
   Float_t mBbcWestRate;
   Float_t mZdcEastRate;
   Float_t mZdcWestRate;
+
   //Nov.10, 2008, Na
   UShort_t mVpd[64];
   UShort_t mZdcSumAdcEast;
@@ -143,35 +153,34 @@ protected: //these are written out
   UShort_t mZdcSmdEastVertical[8];
   UShort_t mZdcSmdWestHorizontal[8];
   UShort_t mZdcSmdWestVertical[8];
-  Float_t mSpaceCharge;
-
-  UShort_t mbTofTrayMultiplicity ; // BTOF tray multiplicity
-  UShort_t mNumberOfGlobalTracks ; // # of global tracks
-
-  // From StMuPrimaryVertex
-  Float_t mRanking ;
-  UShort_t mNBEMCMatch ;
-  UShort_t mNBTOFMatch ;
 
   // BBC ADC for q-vectors (Hiroshi)
   UShort_t mBbcAdcEast[24] ; /// BBC East ADC: 0-23
   UShort_t mBbcAdcWest[24] ; /// BBC West ADC: 24-47
 
+  Float_t mSpaceCharge;
+
   // Online HT/JP thresholds
   UChar_t mHT_Th[4];
   UChar_t mJP_Th[4];
-    
+
   friend class StPicoDst;
   friend class StPicoDstMaker;
-    
+
   ClassDef(StPicoEvent,1)
 };
 inline Int_t    StPicoEvent::eventId() const          { return mEventId; }
 inline Int_t    StPicoEvent::fillId() const           { return (Int_t)mFillId; }
 inline Float_t  StPicoEvent::bField() const           { return mBField; }
+
 inline StThreeVectorF const & StPicoEvent::primaryVertex() const { return mPrimaryVertex; }
 inline StThreeVectorF const & StPicoEvent::primaryVertexError() const { return mPrimaryVertexError; }
+inline Float_t  StPicoEvent::ranking() const { return mRanking ; }
+inline UShort_t StPicoEvent::nBEMCMatch() const { return mNBEMCMatch ; }
+inline UShort_t StPicoEvent::nBTOFMatch() const { return mNBTOFMatch ; }
+
 inline std::vector<unsigned int> StPicoEvent::triggerIds() const { return mTriggerIds;}
+
 inline Int_t    StPicoEvent::refMultPos() const       { return (Int_t)mRefMultPos; }
 inline Int_t    StPicoEvent::refMultNeg() const       { return (Int_t)mRefMultNeg; }
 inline Int_t    StPicoEvent::refMultFtpcEast() const  { return (Int_t)mRefMultFtpcEast; }
@@ -191,14 +200,17 @@ inline Int_t    StPicoEvent::refMult2West() const       { return (Int_t)(mRefMul
 inline Int_t    StPicoEvent::refMult2() const           { return (Int_t)(mRefMult2PosEast+mRefMult2NegEast+mRefMult2PosWest+mRefMult2NegWest); }
 inline Int_t    StPicoEvent::refMultHalfEast() const    { return (Int_t)(mRefMultHalfPosEast+mRefMultHalfNegEast); }
 inline Int_t    StPicoEvent::refMultHalfWest() const    { return (Int_t)(mRefMultHalfPosWest+mRefMultHalfNegWest); }
+
 inline Int_t    StPicoEvent::grefMult() const           { return (Int_t)(mGRefMult); }
+inline UShort_t StPicoEvent::numberOfGlobalTracks() const { return mNumberOfGlobalTracks ; }
+inline UShort_t StPicoEvent::btofTrayMultiplicity() const { return mbTofTrayMultiplicity ; }
 inline Int_t    StPicoEvent::numberOfPxlInnerHits() const { return (Int_t)(mNHitsHFT[0]); }
 inline Int_t    StPicoEvent::numberOfPxlOuterHits() const { return (Int_t)(mNHitsHFT[1]); }
 inline Int_t    StPicoEvent::numberOfIstHits() const      { return (Int_t)(mNHitsHFT[2]); }
 inline Int_t    StPicoEvent::numberOfSsdHits() const      { return (Int_t)(mNHitsHFT[3]); }
 
 inline Int_t    StPicoEvent::nVpdHitsEast() const     { return (Int_t)mNVpdHitsEast; }
-inline Int_t    StPicoEvent::nVpdHitsWest() const     { return (Int_t)mNVpdHitsWest; } 
+inline Int_t    StPicoEvent::nVpdHitsWest() const     { return (Int_t)mNVpdHitsWest; }
 inline Int_t    StPicoEvent::nT0() const              { return (Int_t)mNT0; }
 inline Float_t  StPicoEvent::vzVpd() const            { return (Float_t)mVzVpd/100.; }
 
@@ -216,25 +228,18 @@ inline Float_t StPicoEvent::backgroundRate() const             { return mBackgro
 inline Float_t StPicoEvent::bbcBlueBackgroundRate() const      { return mBbcBlueBackgroundRate; }
 inline Float_t StPicoEvent::bbcYellowBackgroundRate() const    { return mBbcYellowBackgroundRate; }
 inline Float_t StPicoEvent::bbcEastRate() const { return mBbcEastRate; }
-inline Float_t StPicoEvent::bbcWestRate() const { return mBbcWestRate; }  
+inline Float_t StPicoEvent::bbcWestRate() const { return mBbcWestRate; }
 inline Float_t StPicoEvent::zdcEastRate() const { return mZdcEastRate; }
 inline Float_t StPicoEvent::zdcWestRate() const { return mZdcWestRate; }
 
-inline Float_t StPicoEvent::spaceCharge() const { return mSpaceCharge; }
-
-inline UShort_t StPicoEvent::btofTrayMultiplicity() const { return mbTofTrayMultiplicity ; }
-inline UShort_t StPicoEvent::numberOfGlobalTracks() const { return mNumberOfGlobalTracks ; }
-
-inline Float_t  StPicoEvent::ranking() const { return mRanking ; }
-inline UShort_t StPicoEvent::nBEMCMatch() const { return mNBEMCMatch ; }
-inline UShort_t StPicoEvent::nBTOFMatch() const { return mNBTOFMatch ; }
-
 inline UShort_t StPicoEvent::bbcAdcEast(const Int_t i) const { return mBbcAdcEast[i]; }
 inline UShort_t StPicoEvent::bbcAdcWest(const Int_t i) const { return mBbcAdcWest[i]; }
+
+inline Float_t StPicoEvent::spaceCharge() const { return mSpaceCharge; }
 
 inline Int_t   StPicoEvent::ht_th(const Int_t i) const { return mHT_Th[i]; }
 inline Int_t   StPicoEvent::jp_th(const Int_t i) const { return mJP_Th[i]; }
 
 inline void StPicoEvent::setHT_Th(const Int_t i, const Int_t th) { mHT_Th[i] = (UChar_t)th; }
-inline void StPicoEvent::setJP_Th(const Int_t i, const Int_t th) { mJP_Th[i] = (UChar_t)th; }  
+inline void StPicoEvent::setJP_Th(const Int_t i, const Int_t th) { mJP_Th[i] = (UChar_t)th; }
 #endif
