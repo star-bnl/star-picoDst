f5ac8bc16d38d5fe8f9009efda98678cbd4378f1
Mustafa Mustafa <mmustafa@lbl.gov>
2016-08-30 16:30:30 -0400

    Use a lambda for a shorter code

--- StPicoEvent/StPicoEmcPidTraits.cxx
+++ StPicoEvent/StPicoEmcPidTraits.cxx
@@ -23,22 +23,24 @@ StPicoEmcPidTraits::StPicoEmcPidTraits(Int_t index, Int_t id, Int_t adc0, Float_
 {
   mTrackIndex = (index > std::numeric_limits<short>::max()) ? -1 : (Short_t)index;
 
+  auto getConstrainedShort = [](float x) { return fabs(x) >= std::numeric_limits<short>::max() ? std::numeric_limits<short>::max() : (short)(TMath::Nint(x));};
+
   mBEMCId       = (id > std::numeric_limits<short>::max()) ? -1 : (Short_t)id;
   mBTOWADC0     = (adc0 > std::numeric_limits<unsigned short>::max()) ? std::numeric_limits<unsigned short>::max() : (UShort_t)adc0;
-  mBTOWE0       = (e[0] * 1000. > std::numeric_limits<short>::max()) ? std::numeric_limits<short>::max() : (Short_t)(TMath::Nint(e[0] * 1000.));
-  mBTOWE        = (e[1] * 1000. > std::numeric_limits<short>::max()) ? std::numeric_limits<short>::max() : (Short_t)(TMath::Nint(e[1] * 1000.));
-  mBEMCDistZ    = (fabs(dist[0] * 100.) > std::numeric_limits<short>::max()) ? std::numeric_limits<short>::max() : (Short_t)(TMath::Nint(dist[0] * 100.));
-  mBEMCDistPhi  = (fabs(dist[1] * 10000.) > std::numeric_limits<short>::max()) ? std::numeric_limits<short>::max() : (Short_t)(TMath::Nint(dist[1] * 10000.));
+  mBTOWE0       = getConstrainedShort(e[0] * 1000.);
+  mBTOWE        = getConstrainedShort(e[1] * 1000.);
+  mBEMCDistZ    = getConstrainedShort(dist[0] * 100.);
+  mBEMCDistPhi  = getConstrainedShort(dist[1] * 10000.);
   mBSMDNEta     = (nhit[0] > std::numeric_limits<unsigned char>::max()) ? std::numeric_limits<unsigned char>::max() : (UChar_t)(nhit[0]);
   mBSMDNPhi     = (nhit[1] > std::numeric_limits<unsigned char>::max()) ? std::numeric_limits<unsigned char>::max() : (UChar_t)(nhit[1]);
 
   mBTOWId       = (ntow[0] <= 0 || ntow[0] > 4800) ? -1 : (Short_t)ntow[0];
   mBTOWId23     = (ntow[1] < 0 || ntow[1] >= 9 || ntow[2] < 0 || ntow[2] >= 9) ? -1 : (Char_t)(ntow[1] * 10 + ntow[2]);
-  mBTOWE1       = (e[2] * 1000. > std::numeric_limits<short>::max()) ? std::numeric_limits<short>::max() : (Short_t)(TMath::Nint(e[2] * 1000.));
-  mBTOWE2       = (e[3] * 1000. > std::numeric_limits<short>::max()) ? std::numeric_limits<short>::max() : (Short_t)(TMath::Nint(e[3] * 1000.));
-  mBTOWE3       = (e[4] * 1000. > std::numeric_limits<short>::max()) ? std::numeric_limits<short>::max() : (Short_t)(TMath::Nint(e[4] * 1000.));
-  mBTOWDistEta  = (fabs(dist[2] * 10000.) > std::numeric_limits<short>::max()) ? std::numeric_limits<short>::max() : (Short_t)(TMath::Nint(dist[2] * 10000.));
-  mBTOWDistPhi  = (fabs(dist[3] * 10000.) > std::numeric_limits<short>::max()) ? std::numeric_limits<short>::max() : (Short_t)(TMath::Nint(dist[3] * 10000.));
+  mBTOWE1       = getConstrainedShort(e[2] * 1000.);
+  mBTOWE2       = getConstrainedShort(e[3] * 1000.);
+  mBTOWE3       = getConstrainedShort(e[4] * 1000.);
+  mBTOWDistEta  = getConstrainedShort(dist[2] * 10000.);
+  mBTOWDistPhi  = getConstrainedShort(dist[3] * 10000.);
 }
 
 //----------------------------------------------------------------------------------
