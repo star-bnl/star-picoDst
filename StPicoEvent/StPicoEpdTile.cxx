3df6df069b0edede85e30e4244c5f31b8b3b5682
Dmitri Smirnov <d.s@plexoos.com>
2017-06-16 00:47:44 -0400

    StPicoBbcTile: Keep functionality relevant to EPD, remove BBC

--- StPicoEvent/StPicoEpdTile.cxx
+++ StPicoEvent/StPicoEpdTile.cxx
@@ -1,29 +1,19 @@
 #include "St_base/StMessMgr.h"
-#include "StPicoEvent/StPicoBbcEpdTile.h"
+#include "StPicoEvent/StPicoEpdTile.h"
 
-StPicoBbcEpdTile::StPicoBbcEpdTile() : mId(0), mQTdata(0)
+StPicoEpdTile::StPicoEpdTile() : mId(0), mQTdata(0)
 {
   /* no-op */
 }
 
-StPicoBbcEpdTile::StPicoBbcEpdTile(Short_t ID, Short_t ADC, Short_t TAC, Short_t TDC, Bool_t hasTAC)
+StPicoEpdTile::StPicoEpdTile(Short_t PP, Short_t TT, Short_t EW, Short_t ADC, Short_t TAC, Short_t TDC, Bool_t hasTAC)
 {
-  mId = ID;
+  this->setPPandTTandEW(PP, TT, EW);
   this->setQTdata(ADC, TAC, TDC, hasTAC);
-
-  /*
-    if (ADC!=0) cout << "--------------";
-    cout << "Tile being created with ID=" << mId
-    << " ADC=" << ADC << " TDC=" << TDC << " TAC=" << TAC << endl;
-    cout << "Ampersand gives " << (ADC&0xFFF) << endl;
-    cout << "shifing one gives " << (ADC&0xFFF)<<1 << endl;
-    cout << "My method says ADC is " << this->adc() << " TDC is " << this->tdc() << " TAC is " << this->tac() << endl;
-    cout << "mQTdata is " << mQTdata << endl;
-  */
 }
 
 
-void StPicoBbcEpdTile::setQTdata(Short_t ADC, Short_t TAC, Short_t TDC, Bool_t hasTAC)
+void StPicoEpdTile::setQTdata(Short_t ADC, Short_t TAC, Short_t TDC, Bool_t hasTAC)
 {
   ULong_t uADC = ADC;
   ULong_t uTDC = TDC;
@@ -31,7 +21,7 @@ void StPicoBbcEpdTile::setQTdata(Short_t ADC, Short_t TAC, Short_t TDC, Bool_t h
   mQTdata = (uADC & 0xFFF) + ((uTAC & 0xFFF) << 12) + ((uTDC & 0x1F) << 24) + (hasTAC ? (1 << 29) : 0);
 }
 
-void StPicoBbcEpdTile::Print(const Char_t *option) const
+void StPicoEpdTile::Print(const Char_t *option) const
 {
 
   /*  --- this below does not compile so I am skipping it.
--- StPicoEvent/StPicoEpdTile.h
+++ StPicoEvent/StPicoEpdTile.h
@@ -1,5 +1,5 @@
 /*
-   EPD and BBC tile class for STAR picoDst
+   EPD tile class for STAR picoDst
    Total size of this object is 6 bytes
 
    If all BBC inner tiles are saved, this makes 2*16*6 = 192 bytes
@@ -10,62 +10,56 @@
    * in 2017, we will save all EPD tiles.  After 2017, we will not.
    - Mike Lisa 20 May 2017
 */
-#ifndef StPicoBbcEpdTile_h
-#define StPicoBbcEpdTile_h
+#ifndef StPicoEpdTile_h
+#define StPicoEpdTile_h
 
 #include "TObject.h"
 
-class StPicoBbcEpdTile : public TObject
+class StPicoEpdTile : public TObject
 {
 public:
 
-  StPicoBbcEpdTile();
-  StPicoBbcEpdTile(Short_t ID, Short_t ADC, Short_t TAC, Short_t TDC, Bool_t hasTAC);
+  StPicoEpdTile();
+  StPicoEpdTile(Short_t PP, Short_t TT, Short_t EW, Short_t ADC, Short_t TAC, Short_t TDC, Bool_t hasTAC);
 
   virtual void Print(const Char_t *option = "") const;
 
-  void setId(Short_t ID);
   void setQTdata(Short_t ADC, Short_t TAC, Short_t TDC, Bool_t hasTAC);
+  void setPPandTTandEW(Short_t PP, Short_t TT, Short_t EW);
 
   Bool_t hasTAC() const;
   Short_t adc() const;
   Short_t tac() const;
   Short_t tdc() const;
-  Bool_t isBbc() const;
-  Bool_t isEpd() const;
   Bool_t isEast() const;
   Short_t id() const;
 
-  Short_t BbcPmtNumber() const;  // 1...32
-  Short_t EpdPP() const;         // 1...12
-  Short_t EpdTT() const;         // 1...31
+  Short_t ID() const;
+  Short_t PP() const;         // 1...12
+  Short_t TT() const;         // 1...31
 
 protected:
   // mId:
   // * for EPD: 100*PP+TT
-  // * for BBC: Phototube #
-  // * sign for both, +/- = West/East
+  // * sign: +/- = West/East
   // PP, TT, Phototube indices start at 1, not zero.
   Short_t mId;
   ULong_t mQTdata; // bits 0-11 are ADC;  bits 12-23 are TAC;  bits 24-28 are TDC;  bit 29 is noTAC flag
 
-  ClassDef(StPicoBbcEpdTile, 1)
+  ClassDef(StPicoEpdTile, 1)
 };
 
-inline void StPicoBbcEpdTile::setId(Short_t ID) {mId = ID;}
+inline void    StPicoEpdTile::setPPandTTandEW(Short_t PP, Short_t TT, Short_t EW) {mId = 100 * PP + TT; if (EW < 0) {mId *= -1;};}
+inline Bool_t  StPicoEpdTile::isEast() const {return (mId < 0);}
+inline Short_t StPicoEpdTile::id() const {return mId;}
 
-inline Bool_t  StPicoBbcEpdTile::isEast() const {return (mId < 0);}
-inline Bool_t  StPicoBbcEpdTile::isBbc() const {return (abs(mId) < 100);}
-inline Bool_t  StPicoBbcEpdTile::isEpd() const {return (abs(mId) > 100);}
-inline Short_t StPicoBbcEpdTile::id() const {return mId;}
+inline Short_t StPicoEpdTile::ID() const {return mId;}
+inline Short_t StPicoEpdTile::PP() const {return abs(mId) > 100 ? abs(mId / 100) : -999;}
+inline Short_t StPicoEpdTile::TT() const {return abs(mId) > 100 ? abs(mId % 100) : -999;}
 
-inline Short_t StPicoBbcEpdTile::BbcPmtNumber() const {return abs(mId) < 100 ? abs(100) : -999;}
-inline Short_t StPicoBbcEpdTile::EpdPP() const {return abs(mId) > 100 ? abs(mId / 100) : -999;}
-inline Short_t StPicoBbcEpdTile::EpdTT() const {return abs(mId) > 100 ? abs(mId % 100) : -999;}
-
-inline Short_t StPicoBbcEpdTile::adc() const {Short_t x = (mQTdata & 0x0FFF); return x;}
-inline Short_t StPicoBbcEpdTile::tac() const {Short_t x = ((mQTdata >> 12) & 0x0FFF); return x;}
-inline Short_t StPicoBbcEpdTile::tdc() const {Short_t x = ((mQTdata >> 24) & 0x001F); return x;}
-inline Bool_t  StPicoBbcEpdTile::hasTAC() const {return ((mQTdata >> 29 & 0x1) != 0);}
+inline Short_t StPicoEpdTile::adc() const {Short_t x = (mQTdata & 0x0FFF); return x;}
+inline Short_t StPicoEpdTile::tac() const {Short_t x = ((mQTdata >> 12) & 0x0FFF); return x;}
+inline Short_t StPicoEpdTile::tdc() const {Short_t x = ((mQTdata >> 24) & 0x001F); return x;}
+inline Bool_t  StPicoEpdTile::hasTAC() const {return ((mQTdata >> 29 & 0x1) != 0);}
 
 #endif
