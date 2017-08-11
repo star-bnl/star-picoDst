695a410ea8701624c25460c12cfb9f7cdfc96b32
Dmitri Smirnov <d.s@plexoos.com>
2017-08-11 13:08:10 -0400

    Corrected code formatting with 'astyle -s2 -p -H -A3 -k3 -O -o -y -Y -f'

--- StPicoEvent/StPicoFmsHit.h
+++ StPicoEvent/StPicoFmsHit.h
@@ -13,40 +13,41 @@
 #include "StarClassLibrary/StThreeVectorF.hh"
 
 
-class StPicoFmsHit:public TObject{
- public:
+class StPicoFmsHit: public TObject
+{
+public:
   StPicoFmsHit()
   {
-    set(0,0,0);
+    set(0, 0, 0);
   }
 
   StPicoFmsHit(UShort_t detectorId, UShort_t channel, UShort_t adc)
   {
-    set(detectorId,channel,adc);
+    set(detectorId, channel, adc);
   }
 
-  virtual ~StPicoFmsHit(){}
+  virtual ~StPicoFmsHit() {}
 
-  void Print(const Char_t* option="") const;
+  void Print(const Char_t *option = "") const;
 
   void set(UShort_t detectorId, UShort_t channel, UShort_t adc)
   {
-    encodeDetectorIdChannel(detectorId,channel);
-    mAdc=adc;
+    encodeDetectorIdChannel(detectorId, channel);
+    mAdc = adc;
   }
 
   void encodeDetectorIdChannel(UShort_t detectorId, UShort_t channel)
   {
-    mChannelDetectorId=32*channel+detectorId;
+    mChannelDetectorId = 32 * channel + detectorId;
   }
 
   UShort_t detectorId() const
   {
-    return mChannelDetectorId%32;
+    return mChannelDetectorId % 32;
   }
   UShort_t channel() const
   {
-    return mChannelDetectorId/32;
+    return mChannelDetectorId / 32;
   }
   UShort_t adc() const
   {
@@ -57,7 +58,7 @@ class StPicoFmsHit:public TObject{
   UShort_t mChannelDetectorId;//32*channel+detectorId. Allow channel 0-2047, detectorId 0-31.
   UShort_t mAdc;
 
-  ClassDef(StPicoFmsHit,1)
+  ClassDef(StPicoFmsHit, 1)
 };
 
 #endif
