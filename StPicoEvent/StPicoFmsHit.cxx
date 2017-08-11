1a3b1b3051290775d0c06cfdcded161edfb8780f
Dmitri Smirnov <d.s@plexoos.com>
2017-08-11 14:47:20 -0400

    Consistency with other picoEvent classes

--- StPicoEvent/StPicoFmsHit.cxx
+++ StPicoEvent/StPicoFmsHit.cxx
@@ -1,10 +1,21 @@
+#include "StPicoEvent/StPicoFmsHit.h"
 
 
 /**
  * \author: Peifeng Liu, Stony Brook University, pliuphys@gmail.com
  */
+StPicoFmsHit::StPicoFmsHit() : TObject(), mChannelDetectorId(0), mAdc(0)
+{
+}
 
-#include "StPicoFmsHit.h"
+
+StPicoFmsHit::StPicoFmsHit(int detectorId, int channelId, int adc) :
+  TObject(),
+  mChannelDetectorId(32*channelId + detectorId),
+  mAdc(adc)
+{
+}
+
 
 void StPicoFmsHit::Print(const Char_t *option) const
 {
