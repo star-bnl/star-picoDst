f0982e16059400ddeee4a3d4be6561e3d0cbbb96
Dmitri Smirnov <d.s@plexoos.com>
2017-08-11 13:08:10 -0400

    Corrected code formatting with 'astyle -s2 -p -H -A3 -k3 -O -o -y -Y -f'

--- StPicoEvent/StPicoFmsHit.cxx
+++ StPicoEvent/StPicoFmsHit.cxx
@@ -6,10 +6,10 @@
 
 #include "StPicoFmsHit.h"
 
-void StPicoFmsHit::Print(const Char_t* option) const
+void StPicoFmsHit::Print(const Char_t *option) const
 {
-  LOG_INFO<<"DetectorId = "<<detectorId()
-          <<"Channel = "   <<channel()
-          <<"ADC = "       <<adc()
-          <<endm;
+  LOG_INFO << "DetectorId = " << detectorId()
+           << "Channel = "   << channel()
+           << "ADC = "       << adc()
+           << endm;
 }
