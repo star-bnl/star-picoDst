bd47a61421a0504e6bb6e046910626d1f7747928
Dmitri Smirnov <d.s@plexoos.com>
2018-05-09 13:06:30 -0400

    cmake: Add local LinkDef.h

---
+++ StPicoEvent/LinkDef.h
@@ -0,0 +1,23 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class StPicoBEmcPidTraits+;
+#pragma link C++ class StPicoBTofHit+;
+#pragma link C++ class StPicoBTofPidTraits+;
+#pragma link C++ class StPicoBTowHit+;
+#pragma link C++ class StPicoBbcHit+;
+#pragma link C++ class StPicoEmcTrigger+;
+#pragma link C++ class StPicoEpdHit+;
+#pragma link C++ class StPicoEvent+;
+#pragma link C++ class StPicoFmsHit+;
+#pragma link C++ class StPicoMtdHit+;
+#pragma link C++ class StPicoMtdPidTraits+;
+#pragma link C++ class StPicoMtdTrigger+;
+#pragma link C++ class StPicoTrack++;
+#pragma link C++ class StThreeVector<float>+;
+#pragma link C++ class StThreeVector<double>+;
+
+#endif
